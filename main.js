6a277f13e1dc4791ddbc21919691c3cb316de05e
shahin safaroff <shahinsafaroff@gmail.com>
2020-12-25 16:12:35 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+let countDisplay = document.querySelector('.count-display');
+let incrementButton = document.querySelector('.increment-button')
+
+function incrementCount() {
+    let currentCount=parseInt(countDisplay.textContent);
+    countDisplay.textContent = currentCount + 1;
+ };
+
+ incrementButton.addEventListener('click', incrementCount);
+});
